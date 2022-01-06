81839da83a62f04f5d3597210d4c385d6b6c8685
IsaquePorto <isaqueporto2016@gmail.com>
2022-01-06 13:53:53 -0300

    finalizando as classes de domínio

--- src/main/java/com/isaque/bookstore/domain/Categoria.java
+++ src/main/java/com/isaque/bookstore/domain/Categoria.java
@@ -1,17 +1,28 @@
 package com.isaque.bookstore.domain;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
 import lombok.Data;
 
-@Data
-public class Categoria {
-	 
+@Data @Entity
+public class Categoria implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String nome;
 	private String descricao;
 	
+	@OneToMany(mappedBy = "categoria")
 	private List<Livro> livros = new ArrayList<Livro>();
 	
 	public Categoria () {
--- src/main/java/com/isaque/bookstore/domain/Livro.java
+++ src/main/java/com/isaque/bookstore/domain/Livro.java
@@ -1,15 +1,28 @@
 package com.isaque.bookstore.domain;
 
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
 import lombok.Data;
 
-@Data
-public class Livro {
+@Data @Entity 
+public class Livro implements Serializable {
 	
+	private static final long serialVersionUID = 1L;
+	
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String titulo;
 	private String nomeAutor;
 	private String texto;
 	
+	@ManyToOne @JoinColumn(name = "categoria_id")
 	private Categoria categoria;
 	
 	public Livro () {
