0f44ee48382801117e0e1baf38e85a043c15d2f8
IsaquePorto <isaqueporto2016@gmail.com>
2022-01-09 21:48:46 -0300

    Tratando exceções do domain Categoria

---
+++ src/main/java/com/isaque/bookstore/resource/exceptions/StandardError.java
@@ -0,0 +1,13 @@
+package com.isaque.bookstore.resource.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor @NoArgsConstructor @Data
+public class StandardError {
+	
+	private Long timestamp;
+	private Integer status;
+	private String error;
+}
--- src/main/java/com/isaque/bookstore/service/CategoriaService.java
+++ src/main/java/com/isaque/bookstore/service/CategoriaService.java
@@ -7,18 +7,18 @@ import org.springframework.stereotype.Service;
 
 import com.isaque.bookstore.domain.Categoria;
 import com.isaque.bookstore.repository.CategoriaRepository;
+import com.isaque.bookstore.service.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repository;
-	
+
 	public Categoria findById(int id) {
 		Optional<Categoria> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
-	
-	
-	
+
 }
