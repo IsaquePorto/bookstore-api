63b4132f8c22fd1c5a87e6611e767b920e70a7ce
IsaquePorto <isaqueporto2016@gmail.com>
2022-02-28 20:52:33 -0300

    Troca de BD

--- src/main/java/com/isaque/bookstore/config/DevConfig.java
+++ src/main/java/com/isaque/bookstore/config/DevConfig.java
@@ -1,7 +1,6 @@
 package com.isaque.bookstore.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -11,18 +10,17 @@ import com.isaque.bookstore.service.BDService;
 @Configuration
 @Profile("dev")
 public class DevConfig {
-	
+
 	@Autowired
 	private BDService bdService;
-	
-	@Value("${spring.jpa.hibernate.ddl-auto}")
-	private String strategy;
-	
+
+	//@Value("${spring.jpa.hibernate.ddl-auto}")
+	//private String strategy;
+
 	@Bean
-	public boolean instanciaBancoDeDados() {
-		if(strategy.equals("create")) {
-			bdService.instanciaBancoDeDados();
-		}
-		return false;
+	public void instanciaBancoDeDados() {
+
+		this.bdService.instanciaBancoDeDados();
+
 	}
 }
